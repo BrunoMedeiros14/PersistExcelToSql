fa8e53aa7dabc06c3a1f63e70bbf40cec9983c62
Bruno <brunodemedeiros14@gmail.com>
2023-06-06 20:01:57 -0300

    feat(back-end): init spring

---
+++ backend/excel-to-sql/src/main/java/com/medeiros/excel/to/sql/ExcelToSqlApplication.java
@@ -0,0 +1,13 @@
+package com.medeiros.excel.to.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelToSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelToSqlApplication.class, args);
+	}
+
+}
